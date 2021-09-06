85a8b255c60ad18b9a8d536442ba74da345f824f
ArtemOshman <kraffo61@gmail.com>
2021-09-06 11:19:54 +0300

    Background gradient with raycasting, added basic camera logic

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,43 @@
 #include <color.h>
 #include <ray.h>
 #include <iostream>
+
+color ray_color(const ray& r){
+    //scale to (-1.0 1.0)
+    vec3 unit_direction = unit_vector(r.direction());
+    //scale to (0.0 1.0)
+    auto t = 0.5 *(unit_direction.y() + 1);
+    //lerp between white and blue
+    return color((1-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.0, 0.0, 1.0));
+}
+
 int main()
 {
-    const int img_height = 256;
-    const int img_width = 256;
+    //Image data
+    const auto aspect_ratio = 16.0/9.0;
+    const int img_width = 400;
+    const int img_height = static_cast<int>(img_width * aspect_ratio);
 
+    //Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * aspect_ratio;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+
+    //Render
     std::cout << "P3 \n" << img_width << " " << img_height << std::endl << "255 \n";
     color pixel_color;
     for(int j = img_height-1; j >= 0; j--){
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for(int i = 0; i < img_width; i++){
-            pixel_color = color((double)j/img_height, (double)i/img_width, (double)0.1);
+            auto u = double(i)/(img_width-1);
+            auto v = double(j)/(img_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
